f834b92906f555ea871b35b5622c7b102539507a
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-07-09 21:17:54 +0200

    make get request.

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const http = require('http')
-const hostname = '127.0.0.1'
-const port = 3000
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World\n')
-})
+// const http = require('http')
+// const hostname = '127.0.0.1'
+// const port = 3000
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.end('Hello World von Friedrich\n')
+// })
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`)
+// })
+const fs = require('fs');
+
+const https = require('https')
+const options = { 
+    hostname: 'example.com',
+    port: 443, 
+    path: '/todos', 
+    method: 'GET', 
+}
+
+const req = https.request(options, res => { 
+    console.log(`statusCode: ${res.statusCode}`)
+
 })
