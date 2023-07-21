a6e856060ca82287811609f7884fc277b38570be
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-07-21 21:25:41 +0200

    Create a file and learn about URL Object.

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
-const axios = require('axios');
 const fs = require('fs')
 const path = require('path');
 
 
-fs.mkdir(path.join(__dirname, 'test'), (err) => {
-    if (err) {
-        return console.error(err);
-    }
-    console.log('Directory created successfully!');
+const content = 'Some content!';
+
+fs.writeFile('/home/friedrichtenhagen/node-server/test.txt', content, err => {
+  if (err) {
+    console.error(err);
+  }
+  // file written successfully
 });
-
-axios
-  .get('https://example.com/todos')
-  .then(res => {
-    console.log(`statusCode: ${res.status}`);
-    console.log(res);
-  })
-  .catch(error => {
-    console.error(error);
-  });
-
